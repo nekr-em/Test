8eacee7b5af7d6127569890dcae3a7ad26d30f56
Ekaterina1706 <em585788@gmail.com>
2022-11-16 18:59:52 +0300

    Добавила компонент HeaderBar.vue

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app-bar
+      app
+      color="primary"
+      dark
+  >
+    <div class="d-flex align-center">
+      <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+      />
+      <span>Некрасова Екатерина</span>
+    </div>
+    <span class="ml-auto mr-2">Тестовое задание</span>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "HeaderBar"
+}
+</script>
+
+<style scoped>
+
+</style>
