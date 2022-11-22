861380f431560de54f8e9d757c3f7fce19335779
Ekaterina1706 <em585788@gmail.com>
2022-11-22 18:58:14 +0300

    Исправила ошибку

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 
-Vue.prototype.$apiUrl = "https://crudcrud.com/api/86acc90293e94f89996f2b8b04e04d15/people"
+Vue.prototype.$apiUrl = "https://crudcrud.com/api/86acc90293e94f89996f2b8b04e04d15/people/"
 Vue.config.productionTip = false
 
 new Vue({
